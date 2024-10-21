f803d39e12187c967e64b4bd25c91f073333ec4a
Yaseen Akram C K <yaseen.61079421@ltimindtree.com>
2024-10-21 14:48:19 +0530

    Test1

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const serverless = require('serverless-http')
+const app = express()
+const port =3000;
+
+app.get('/checkserver', function (req, res) {
+  res.send('Hello World express server is up')
+})
+
+// app.listen(port,()=>{
+//     console.log(`The Nodejs Server is Up and Running in the PORT${port}`);
+// })
+
+export const handler = serverless(app);
